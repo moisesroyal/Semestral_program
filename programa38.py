f53e273eb665d10f77c641896e5dee0fca8f994d
moisesroyal <137737075+moisesroyal@users.noreply.github.com>
2023-07-04 16:24:37 -0500

    programa38.py
    Programa que llama a todos los programas

--- programa38.py
+++ programa38.py
@@ -1,1 +1,1 @@
-import programa6
+import programa1,programa2,programa3,programa4,programa5,programa6,programa7,programa8,programa9,Programa10,Programa11,programa12,programa13,programa14,programa15,programa16,programa17,programa18,programa19,programa20,programa21,programa22,programa23,programa24,programa25,programa26,programa27,programa28,programa29,programa30,programa31,programa32,programa33,programa34,programa35,programa36,programa37
