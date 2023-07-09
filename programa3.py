cbff7b606f9b2f78e522774e9e949f2989d3a2a8
moisesroyal <137737075+moisesroyal@users.noreply.github.com>
2023-07-09 18:26:04 -0500

    Update programa3.py

--- programa3.py
+++ programa3.py
@@ -1,10 +1,9 @@
 
-def suma_division(A,B,C):    
-    D = (A + B + C) / 3
-    return D
+def suma_division():    
 
-A = float (input("Leer A: "))
-B = float (input("Leer B: "))
-C = float (input("Leer C: "))
-D = suma_division(A,B,C)
-print (D)
+    A = float (input("Leer A: "))
+    B = float (input("Leer B: "))
+    C = float (input("Leer C: "))
+    D = (A + B + C) / 3
+    print (D)
+suma_division()
